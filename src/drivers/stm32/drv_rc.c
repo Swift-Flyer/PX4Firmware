2b66442fa9043b45e6451e6a4ca1ee78c3b85a65
Maxim Strinzha <mstrinzha@gmail.com>
2014-05-14 17:02:55 +0400

    DSM and PPMSUM support

--- src/drivers/stm32/drv_rc.c
+++ src/drivers/stm32/drv_rc.c
@@ -13,7 +13,7 @@
 #define PPM_MIN_CHANNEL_VALUE	800		/* shortest valid channel signal */
 #define PPM_MAX_CHANNEL_VALUE	2200		/* longest valid channel signal */
 
-#if 0
+#if 1
 
 #define HRT_TIMER_BASE		STM32_TIM3_BASE
 #define HRT_TIMER_POWER_REG	STM32_RCC_APB1ENR
@@ -59,15 +59,6 @@
 #endif
 
 #if 1
-/*#define GPIO_PPM_CH1_IN (GPIO_ALT|GPIO_AF2|GPIO_SPEED_50MHz|GPIO_PULLUP|GPIO_PORTD|GPIO_PIN12)
-#define GPIO_PPM_CH2_IN (GPIO_ALT|GPIO_AF2|GPIO_SPEED_50MHz|GPIO_PULLUP|GPIO_PORTD|GPIO_PIN13)
-#define GPIO_PPM_CH3_IN (GPIO_ALT|GPIO_AF2|GPIO_SPEED_50MHz|GPIO_PULLUP|GPIO_PORTD|GPIO_PIN14)
-#define GPIO_PPM_CH4_IN (GPIO_ALT|GPIO_AF2|GPIO_SPEED_50MHz|GPIO_PULLUP|GPIO_PORTD|GPIO_PIN15)
-#define GPIO_PPM_CH5_IN (GPIO_ALT|GPIO_AF2|GPIO_SPEED_50MHz|GPIO_PULLUP|GPIO_PORTC|GPIO_PIN6) //
-#define GPIO_PPM_CH6_IN (GPIO_ALT|GPIO_AF2|GPIO_SPEED_50MHz|GPIO_PULLUP|GPIO_PORTC|GPIO_PIN7)
-#define GPIO_PPM_CH7_IN (GPIO_ALT|GPIO_AF2|GPIO_SPEED_50MHz|GPIO_PULLUP|GPIO_PORTC|GPIO_PIN8)
-#define GPIO_PPM_CH8_IN (GPIO_ALT|GPIO_AF2|GPIO_SPEED_50MHz|GPIO_PULLUP|GPIO_PORTC|GPIO_PIN9)*/
-
 
 #define GPIO_PPM_CH1_IN (GPIO_ALT|GPIO_AF2|GPIO_SPEED_50MHz|GPIO_PULLUP|GPIO_PORTC|GPIO_PIN9)
 #define GPIO_PPM_CH2_IN (GPIO_ALT|GPIO_AF2|GPIO_SPEED_50MHz|GPIO_PULLUP|GPIO_PORTC|GPIO_PIN8)
@@ -77,13 +68,6 @@
 #define GPIO_PPM_CH6_IN (GPIO_ALT|GPIO_AF2|GPIO_SPEED_50MHz|GPIO_PULLUP|GPIO_PORTD|GPIO_PIN14)
 #define GPIO_PPM_CH7_IN (GPIO_ALT|GPIO_AF2|GPIO_SPEED_50MHz|GPIO_PULLUP|GPIO_PORTD|GPIO_PIN13)
 #define GPIO_PPM_CH8_IN (GPIO_ALT|GPIO_AF2|GPIO_SPEED_50MHz|GPIO_PULLUP|GPIO_PORTD|GPIO_PIN12)
-
-
-
-
-
-
-
 
 #define PPM_TIMER3_VECTOR STM32_IRQ_TIM3
 #define PPM_TIMER3_POWER_REG STM32_RCC_APB1ENR
@@ -142,120 +126,123 @@ static int		rc_tim_isr(int irq, void *context);
 
 void rc_init(void)
 {
-#if 0
-	irq_attach(HRT_TIMER_VECTOR, rc_tim_isr);
+	if(0)
+	{
+		irq_attach(HRT_TIMER_VECTOR, rc_tim_isr);
 
-	/* clock/power on our timer */
-	modifyreg32(HRT_TIMER_POWER_REG, 0, HRT_TIMER_POWER_BIT);
+		/* clock/power on our timer */
+		modifyreg32(HRT_TIMER_POWER_REG, 0, HRT_TIMER_POWER_BIT);
 
-	/* disable and configure the timer */
-	rCR1 = 0;
-	rCR2 = 0;
-	rSMCR = 0;
-	rDIER = DIER_HRT | DIER_PPM;
-	rCCER = 0;		/* unlock CCMR* registers */
-	rCCMR1 = CCMR1_PPM;
-	rCCMR2 = CCMR2_PPM;
-	rCCER = CCER_PPM;
-	rDCR = 0;
+		/* disable and configure the timer */
+		rCR1 = 0;
+		rCR2 = 0;
+		rSMCR = 0;
+		rDIER = DIER_HRT | DIER_PPM;
+		rCCER = 0;		/* unlock CCMR* registers */
+		rCCMR1 = CCMR1_PPM;
+		rCCMR2 = CCMR2_PPM;
+		rCCER = CCER_PPM;
+		rDCR = 0;
 
-	/* configure the timer to free-run at 1MHz */
-	rPSC = (HRT_TIMER_CLOCK / 1000000) - 1;	/* this really only works for whole-MHz clocks */
+		/* configure the timer to free-run at 1MHz */
+		rPSC = (HRT_TIMER_CLOCK / 1000000) - 1;	/* this really only works for whole-MHz clocks */
 
-	/* run the full span of the counter */
-	rARR = 0xffff;
+		/* run the full span of the counter */
+		rARR = 0xffff;
 
-	/* set an initial capture a little ways off */
-	rCCR_HRT = 1000;
+		/* set an initial capture a little ways off */
+		rCCR_HRT = 1000;
 
-	/* generate an update event; reloads the counter, all registers */
-	rEGR = GTIM_EGR_UG;
+		/* generate an update event; reloads the counter, all registers */
+		rEGR = GTIM_EGR_UG;
 
-	/* enable the timer */
-	rCR1 = GTIM_CR1_CEN;
+		/* enable the timer */
+		rCR1 = GTIM_CR1_CEN;
 
-	/* enable interrupts */
-	up_enable_irq(HRT_TIMER_VECTOR);
+		/* enable interrupts */
+		up_enable_irq(HRT_TIMER_VECTOR);
 	
-	ppm_input_init(0xffff);
-	stm32_configgpio(GPIO_PPMSUM_IN);
-#endif
+		ppm_input_init(0xffff);
+		stm32_configgpio(GPIO_PPMSUM_IN);
+	}
+	else
+	{
+		ppm_decoded_channels = 0;
+		ppm_last_valid_decode = 0;
 	
-	ppm_decoded_channels = 0;
-	ppm_last_valid_decode = 0;
-	
-	irq_attach(PPM_TIMER3_VECTOR, rc_tim_isr);
-	irq_attach(PPM_TIMER4_VECTOR, rc_tim_isr);
+		irq_attach(PPM_TIMER3_VECTOR, rc_tim_isr);
+		irq_attach(PPM_TIMER4_VECTOR, rc_tim_isr);
 
-	/* clock/power on our timer */
-	modifyreg32(PPM_TIMER3_POWER_REG, 0, PPM_TIMER3_POWER_BIT);
-	modifyreg32(PPM_TIMER4_POWER_REG, 0, PPM_TIMER4_POWER_BIT);
+		/* clock/power on our timer */
+		modifyreg32(PPM_TIMER3_POWER_REG, 0, PPM_TIMER3_POWER_BIT);
+		modifyreg32(PPM_TIMER4_POWER_REG, 0, PPM_TIMER4_POWER_BIT);
 
-	/* disable and configure the timer */
-	TIM3_rCR1 = 0;
-	TIM4_rCR1 = 0;
+		/* disable and configure the timer */
+		TIM3_rCR1 = 0;
+		TIM4_rCR1 = 0;
 	
-	TIM3_rCR2 = 0;
-	TIM4_rCR2 = 0;
+		TIM3_rCR2 = 0;
+		TIM4_rCR2 = 0;
 	
-	TIM3_rSMCR = 0;
-	TIM4_rSMCR = 0;
+		TIM3_rSMCR = 0;
+		TIM4_rSMCR = 0;
 	
-	TIM3_rDIER = GTIM_DIER_CC1IE | GTIM_DIER_CC2IE | GTIM_DIER_CC3IE | GTIM_DIER_CC4IE;
-	TIM4_rDIER = GTIM_DIER_CC1IE | GTIM_DIER_CC2IE | GTIM_DIER_CC3IE | GTIM_DIER_CC4IE;
+		TIM3_rDIER = GTIM_DIER_CC1IE | GTIM_DIER_CC2IE | GTIM_DIER_CC3IE | GTIM_DIER_CC4IE;
+		TIM4_rDIER = GTIM_DIER_CC1IE | GTIM_DIER_CC2IE | GTIM_DIER_CC3IE | GTIM_DIER_CC4IE;
 	
-	TIM3_rCCER = 0;		/* unlock CCMR* registers */
-	TIM4_rCCER = 0;		/* unlock CCMR* registers */
+		TIM3_rCCER = 0;		/* unlock CCMR* registers */
+		TIM4_rCCER = 0;		/* unlock CCMR* registers */
 	
-	TIM3_rCCMR1 = 0x101;
-	TIM4_rCCMR1 = 0x101;
+		TIM3_rCCMR1 = 0x101;
+		TIM4_rCCMR1 = 0x101;
 	
-	TIM3_rCCMR2 = 0x101;
-	TIM4_rCCMR2 = 0x101;
+		TIM3_rCCMR2 = 0x101;
+		TIM4_rCCMR2 = 0x101;
 	
-	TIM3_rCCER = GTIM_CCER_CC1E | GTIM_CCER_CC2E | GTIM_CCER_CC3E | GTIM_CCER_CC4E 
-				/*| GTIM_CCER_CC1P | GTIM_CCER_CC2P | GTIM_CCER_CC3P | GTIM_CCER_CC4P 
-				| GTIM_CCER_CC1NP | GTIM_CCER_CC2NP | GTIM_CCER_CC3NP | GTIM_CCER_CC4NP*/;
+		TIM3_rCCER = GTIM_CCER_CC1E | GTIM_CCER_CC2E | GTIM_CCER_CC3E | GTIM_CCER_CC4E 
+					/*| GTIM_CCER_CC1P | GTIM_CCER_CC2P | GTIM_CCER_CC3P | GTIM_CCER_CC4P 
+					| GTIM_CCER_CC1NP | GTIM_CCER_CC2NP | GTIM_CCER_CC3NP | GTIM_CCER_CC4NP*/;
 	
-	TIM4_rCCER = GTIM_CCER_CC1E | GTIM_CCER_CC2E | GTIM_CCER_CC3E | GTIM_CCER_CC4E 
-				/*| GTIM_CCER_CC1P | GTIM_CCER_CC2P | GTIM_CCER_CC3P | GTIM_CCER_CC4P 
-				| GTIM_CCER_CC1NP | GTIM_CCER_CC2NP | GTIM_CCER_CC3NP | GTIM_CCER_CC4NP*/;
+		TIM4_rCCER = GTIM_CCER_CC1E | GTIM_CCER_CC2E | GTIM_CCER_CC3E | GTIM_CCER_CC4E 
+					/*| GTIM_CCER_CC1P | GTIM_CCER_CC2P | GTIM_CCER_CC3P | GTIM_CCER_CC4P 
+					| GTIM_CCER_CC1NP | GTIM_CCER_CC2NP | GTIM_CCER_CC3NP | GTIM_CCER_CC4NP*/;
 	
-	TIM3_rDCR = 0;
-	TIM4_rDCR = 0;
+		TIM3_rDCR = 0;
+		TIM4_rDCR = 0;
 
-	/* configure the timer to free-run at 1MHz */
-	TIM3_rPSC = (PPM_TIMER3_CLOCK / 1000000) - 1;	/* this really only works for whole-MHz clocks */
-	TIM4_rPSC = (PPM_TIMER4_CLOCK / 1000000) - 1;	/* this really only works for whole-MHz clocks */
+		/* configure the timer to free-run at 1MHz */
+		TIM3_rPSC = (PPM_TIMER3_CLOCK / 1000000) - 1;	/* this really only works for whole-MHz clocks */
+		TIM4_rPSC = (PPM_TIMER4_CLOCK / 1000000) - 1;	/* this really only works for whole-MHz clocks */
 
-	/* run the full span of the counter */
-	TIM3_rARR = 0xffff;
-	TIM4_rARR = 0xffff;
+		/* run the full span of the counter */
+		TIM3_rARR = 0xffff;
+		TIM4_rARR = 0xffff;
 
-	/* set an initial capture a little ways off */
-	TIM3_rCCR2 = 1000;
-	TIM4_rCCR2 = 1000;
+		/* set an initial capture a little ways off */
+		TIM3_rCCR2 = 1000;
+		TIM4_rCCR2 = 1000;
 
-	/* generate an update event; reloads the counter, all registers */
-	TIM3_rEGR = GTIM_EGR_UG;
-	TIM4_rEGR = GTIM_EGR_UG;
+		/* generate an update event; reloads the counter, all registers */
+		TIM3_rEGR = GTIM_EGR_UG;
+		TIM4_rEGR = GTIM_EGR_UG;
 
-	/* enable the timer */
-	TIM3_rCR1 = GTIM_CR1_CEN;
-	TIM4_rCR1 = GTIM_CR1_CEN;
+		/* enable the timer */
+		TIM3_rCR1 = GTIM_CR1_CEN;
+		TIM4_rCR1 = GTIM_CR1_CEN;
 
-	/* enable interrupts */
-	up_enable_irq(PPM_TIMER3_VECTOR);
-	up_enable_irq(PPM_TIMER4_VECTOR);
+		/* enable interrupts */
+		up_enable_irq(PPM_TIMER3_VECTOR);
+		up_enable_irq(PPM_TIMER4_VECTOR);
 	
-	stm32_configgpio(GPIO_PPM_CH1_IN);
-	stm32_configgpio(GPIO_PPM_CH2_IN);
-	stm32_configgpio(GPIO_PPM_CH3_IN);
-	stm32_configgpio(GPIO_PPM_CH4_IN);
-	stm32_configgpio(GPIO_PPM_CH5_IN);
-	stm32_configgpio(GPIO_PPM_CH6_IN);
-	stm32_configgpio(GPIO_PPM_CH7_IN);
-	stm32_configgpio(GPIO_PPM_CH8_IN);
+		stm32_configgpio(GPIO_PPM_CH1_IN);
+		stm32_configgpio(GPIO_PPM_CH2_IN);
+		stm32_configgpio(GPIO_PPM_CH3_IN);
+		stm32_configgpio(GPIO_PPM_CH4_IN);
+		stm32_configgpio(GPIO_PPM_CH5_IN);
+		stm32_configgpio(GPIO_PPM_CH6_IN);
+		stm32_configgpio(GPIO_PPM_CH7_IN);
+		stm32_configgpio(GPIO_PPM_CH8_IN);
+	}
 }
 
 #define calcWidth(newEdge, oldEdge) ((newEdge < oldEdge) ? newEdge + 0xffff - oldEdge : newEdge-oldEdge) 
@@ -280,95 +267,91 @@ static uint16_t ppm_temp_buffer[PPM_MAX_CHANNELS];
 static int
 rc_tim_isr(int irq, void *context)
 {
-#if 1
-	
-	uint32_t status;
-	uint16_t cTime;
-	uint16_t dTime;
-
-	if(irq == PPM_TIMER3_VECTOR)
+	if(1)
 	{
-		/* copy interrupt status */
-		status = TIM3_rSR;
-		/* ack the interrupts we just read */
-		TIM3_rSR = ~status;
+	
+		uint32_t status;
+		uint16_t cTime;
+		uint16_t dTime;
+
+		if(irq == PPM_TIMER3_VECTOR)
+		{
+			/* copy interrupt status */
+			status = TIM3_rSR;
+			/* ack the interrupts we just read */
+			TIM3_rSR = ~status;
 
 		
-		if(status & GTIM_SR_CC4IF) /* input capture channel1? */
-		{					
-			proceedChannel(TIM3_rCCER, GTIM_CCER_CC4P, TIM3_rCCR4, 0)
-/*			if(TIM3_rCCER & GTIM_CCER_CC4P) //failing
-			{
-				TIM3_rCCER &= ~GTIM_CCER_CC4P;
-				cTime = TIM3_rCCR4;
-				dTime = calcWidth(cTime, ppm_temp_buffer[0]);
-				if (PPM_MIN_CHANNEL_VALUE < dTime && dTime < PPM_MAX_CHANNEL_VALUE) 
-					ppm_buffer[0] = dTime;
+			if(status & GTIM_SR_CC4IF) /* input capture channel1? */
+			{					
+				proceedChannel(TIM3_rCCER, GTIM_CCER_CC4P, TIM3_rCCR4, 0)
+	/*			if(TIM3_rCCER & GTIM_CCER_CC4P) //failing
+				{
+					TIM3_rCCER &= ~GTIM_CCER_CC4P;
+					cTime = TIM3_rCCR4;
+					dTime = calcWidth(cTime, ppm_temp_buffer[0]);
+					if (PPM_MIN_CHANNEL_VALUE < dTime && dTime < PPM_MAX_CHANNEL_VALUE) 
+						ppm_buffer[0] = dTime;
+				}
+				else
+				{
+					ppm_temp_buffer[0] = TIM3_rCCR4;
+					TIM3_rCCER |= GTIM_CCER_CC4P;
+				}*/
 			}
-			else
+			if(status & GTIM_SR_CC3IF) /* input capture channel2? */
 			{
-				ppm_temp_buffer[0] = TIM3_rCCR4;
-				TIM3_rCCER |= GTIM_CCER_CC4P;
-			}*/
-		}
-		if(status & GTIM_SR_CC3IF) /* input capture channel2? */
-		{
-			proceedChannel(TIM3_rCCER, GTIM_CCER_CC3P, TIM3_rCCR3, 1)
-		}
-		if(status & GTIM_SR_CC1IF) /* input capture channel3? */
-		{
-			proceedChannel(TIM3_rCCER, GTIM_CCER_CC1P, TIM3_rCCR1, 2)
+				proceedChannel(TIM3_rCCER, GTIM_CCER_CC3P, TIM3_rCCR3, 1)
+			}
+			if(status & GTIM_SR_CC1IF) /* input capture channel3? */
+			{
+				proceedChannel(TIM3_rCCER, GTIM_CCER_CC1P, TIM3_rCCR1, 2)
 			
-			if(!(TIM3_rCCER & GTIM_CCER_CC1P)) //failing
-			{
-				ppm_decoded_channels = 8;
-				ppm_last_valid_decode = hrt_absolute_time();
-			}			
+				if(!(TIM3_rCCER & GTIM_CCER_CC1P)) //failing
+				{
+					ppm_decoded_channels = 8;
+					ppm_last_valid_decode = hrt_absolute_time();
+				}			
+			}
+			if(status & GTIM_SR_CC2IF) /* input capture channel4? */
+			{					
+				proceedChannel(TIM3_rCCER, GTIM_CCER_CC2P, TIM3_rCCR2, 3)
+			}		
 		}
-		if(status & GTIM_SR_CC2IF) /* input capture channel4? */
-		{					
-			proceedChannel(TIM3_rCCER, GTIM_CCER_CC2P, TIM3_rCCR2, 3)
-		}		
+		else
+		{
+			status = TIM4_rSR;
+			TIM4_rSR = ~status;
+			if(status & GTIM_SR_CC3IF) /* input capture channel5? */
+			{
+				proceedChannel(TIM4_rCCER, GTIM_CCER_CC3P, TIM4_rCCR3, 4)
+			}
+			if(status & GTIM_SR_CC4IF) {					/* input capture channel6? */
+				proceedChannel(TIM4_rCCER, GTIM_CCER_CC4P, TIM4_rCCR4, 5)
+			}
+			if(status & GTIM_SR_CC1IF) /* input capture channel7? */
+			{
+				proceedChannel(TIM4_rCCER, GTIM_CCER_CC1P, TIM4_rCCR1, 6)
+			}
+			if(status & GTIM_SR_CC2IF) /* input capture channel8? */
+			{
+				proceedChannel(TIM4_rCCER, GTIM_CCER_CC2P, TIM4_rCCR2, 7)
+			}
+		
+		}
 	}
 	else
 	{
-		status = TIM4_rSR;
-		TIM4_rSR = ~status;
-		if(status & GTIM_SR_CC3IF) /* input capture channel5? */
-		{
-			proceedChannel(TIM4_rCCER, GTIM_CCER_CC3P, TIM4_rCCR3, 4)
+		uint32_t status;
+		/* copy interrupt status */
+		status = rSR;
+		/* ack the interrupts we just read */
+		rSR = ~status;
+		/* was this a PPM edge? */
+		if (status & (SR_INT_PPM | SR_OVF_PPM)) {
+			/* if required, flip edge sensitivity */
+			ppm_input_decode(status & SR_OVF_PPM, rCCR_PPM);
 		}
-		if(status & GTIM_SR_CC4IF) {					/* input capture channel6? */
-			proceedChannel(TIM4_rCCER, GTIM_CCER_CC4P, TIM4_rCCR4, 5)
-		}
-		if(status & GTIM_SR_CC1IF) /* input capture channel7? */
-		{
-			proceedChannel(TIM4_rCCER, GTIM_CCER_CC1P, TIM4_rCCR1, 6)
-		}
-		if(status & GTIM_SR_CC2IF) /* input capture channel8? */
-		{
-			proceedChannel(TIM4_rCCER, GTIM_CCER_CC2P, TIM4_rCCR2, 7)
-		}
-		
 	}
-#endif	
-	
-#if 0
-	uint32_t status;
-
-	/* copy interrupt status */
-	status = rSR;
-
-	/* ack the interrupts we just read */
-	rSR = ~status;
-
-#if 1
-	/* was this a PPM edge? */
-	if (status & (SR_INT_PPM | SR_OVF_PPM)) {
-		/* if required, flip edge sensitivity */
-		ppm_input_decode(status & SR_OVF_PPM, rCCR_PPM);
-	}
-#endif
-#endif
 	return OK;
 }
